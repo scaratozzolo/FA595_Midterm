9511b605019cd1eb6259f692500d274564270993
Scott Caratozzolo <scaratozzolo12@gmail.com>
2021-10-26 21:29:00 -0400

    remove socketio

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from app import app, socketio
+from app import app
 
 
 if __name__ == '__main__':
 
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
