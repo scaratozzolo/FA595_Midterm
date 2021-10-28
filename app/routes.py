63265c426dbf8d0a05ed7f3fcd9999b0ba039c8e
Scott Caratozzolo <scaratozzolo12@gmail.com>
2021-10-28 10:39:07 -0400

    services routes start

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import jsonify, request
+from torch.cuda.memory import empty_cache
 from app import app
+from app import services
 # This line import your functions from the services folder
 from app.services import *
 
@@ -17,4 +19,35 @@ def nlp():
 
     # Whatever you send to the server will be returned back from the api
     return jsonify(request.json)
+
+@app.route("/nlp/services", methods=["GET"])
+def available_services():
+    
+    services = {"services": {
+                            "all": "/nlp/services/all",
+                            "chat_bot": "/nlp/services/chat_bot",
+                            "next_word": "/nlp/services/next_word",
+        }
+    }
+
+    # Whatever you send to the server will be returned back from the api
+    return jsonify(services)
+
+@app.route("/nlp/services/chat_bot", methods=["POST"])
+def chat_bot_service():
+
+    data = request.json
+    print(data)
+    chat_id = None
+
+    if "text" not in data:
+        return jsonify({"error":"'text' missing from payload"})
+    elif "chat_id" in data:
+        try:
+            chat_id = int(data['chat_data'])
+        except:
+            return jsonify({"error":f"invalid 'chat_id' in payload. given: {data['chat_data']}, type: {type(data['chat_id'])}"})
+
+    return jsonify(chat_bot(text=data['text'], chat_id=chat_id))
+    
 
