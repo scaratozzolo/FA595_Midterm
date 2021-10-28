26c39f77267d0e259ce4280ddb0de13c196812db
Scott Caratozzolo <scaratozzolo12@gmail.com>
2021-10-28 09:43:05 -0400

    move models into functions due to memory issues

--- app/services/scott_services.py
+++ app/services/scott_services.py
@@ -1,16 +1,12 @@
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, AutoModelForCausalLM, AutoTokenizer
 
-# I have no idea is AWS has enough space to hold these models
-GPTtokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-GPTmodel = GPT2LMHeadModel.from_pretrained("gpt2")
-
-DialoGPTtokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
-DialoGPTmodel = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def next_word(text, k=5):
+
+    GPTtokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    GPTmodel = GPT2LMHeadModel.from_pretrained("gpt2")
 
     indexed_tokens = GPTtokenizer.encode(text)
     tokens_tensor = torch.tensor([indexed_tokens])
@@ -23,12 +19,19 @@ def next_word(text, k=5):
 
     probs = predictions[0, -1, :]
     top_next = [GPTtokenizer.decode(i.item()).strip() for i in probs.topk(k)[1]]
+
+    del GPTtokenizer
+    del GPTmodel
+
     return top_next
 
 chat_history = {0:"Placeholder"}
 
 def chat_bot(text, chat_id=None):
     global chat_history
+
+    DialoGPTtokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
+    DialoGPTmodel = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
 
     new_user_input_ids = DialoGPTtokenizer.encode(text + DialoGPTtokenizer.eos_token, return_tensors='pt')
 
@@ -40,6 +43,9 @@ def chat_bot(text, chat_id=None):
     chat_history[chat_id] = DialoGPTmodel.generate(bot_input_ids, max_length=1000, pad_token_id=DialoGPTtokenizer.eos_token_id)
 
     output = DialoGPTtokenizer.decode(chat_history[chat_id][:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+
+    del DialoGPTtokenizer
+    del DialoGPTmodel
 
     return {"response": output, "chat_id": chat_id}
 
