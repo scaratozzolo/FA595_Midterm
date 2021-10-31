189fd5bb69ac56a06fd678060a7ebda85b9358ae
Agathe Sadeghi <55772710+Agatheee@users.noreply.github.com>
2021-10-31 12:24:44 -0400

    Update __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,3 +1,4 @@
 # add your functions here after they've been made
 # from .your_file_name import your_function1, your_function2
 from .scott_services import next_word, chat_bot
+from .agathe_services import word_freq, word_lem
