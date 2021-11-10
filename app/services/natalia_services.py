eeb6cdf957364945e13db3f936d4688a2b121138
Natalia Azarian <nataliaazarian@gmail.com>
2021-11-09 22:57:26 -0500

    correct natalia services

--- app/services/natalia_services.py
+++ app/services/natalia_services.py
@@ -2,10 +2,10 @@ import spacy
 import contextualSpellCheck # pip install contextualSpellCheck
 from textblob import TextBlob
 
-text = "The brown fox laeped over the hole."
-language = 'fr'   
+# text = "The brown fox laeped over the hole."
+# language = 'fr'   
 
-def spellcheck(string):
+def spellcheck(text):
     
     nlp = spacy.load('en_core_web_sm')
     contextualSpellCheck.add_to_pipe(nlp)
@@ -16,7 +16,7 @@ def spellcheck(string):
     
     return {"spellchecked": spelled}
 
-def translate(string, language):
+def translate(text, language):
     
     blob = TextBlob(text)
     
